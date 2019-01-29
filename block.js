9b6c0f1ef1e41236143fea63a65b4411e7e60033
rishivamshi <rishivamshi0079@gmail.com>
2019-01-29 23:16:20 +0530

    created the block class

---
+++ block.js
@@ -0,0 +1,16 @@
+class Block {
+    constructor({timestamp, lastHash, hash, data} ) {
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.hash = hash;
+        this.data = data;
+    }
+}
+
+const Block1 = new Block({
+    timestamp: '01/01/01', 
+    lastHash: 'foo-lastHash',
+    hash: 'foo-Hash', 
+    data: 'foo-data'});
+    
+console.log('block1',Block1);
