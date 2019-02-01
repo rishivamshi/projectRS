82b9ffa49a90bdf849c4bf1091615daff86c2aca
prasadsai <prasadsai0369@gmail.com>
2019-02-01 18:28:17 +0530

    Tests for the blockchain class

--- blockchain.test.js
+++ blockchain.test.js
@@ -1,2 +1,22 @@
 const Block = require('./block');
-cosnt Blockchain = require('./blockchain');
+const Blockchain = require('./blockchain');
+
+describe('Blockchain', () => {
+    const blockchain = new Blockchain();
+
+    it('contains a `chain` array instance' , () => {
+        expect(blockchain.chain instanceof Array).toBe(true);
+    });
+
+    it('starts with the genesis block', () => {
+        expect(blockchain.chain[0]).toEqual(Block.genesis());
+    });
+
+    it('adds a new block to the chain', () => {
+        const newData = 'foo bar';
+        blockchain.addBlock({ data: newData });
+        expect(blockchain.chain[blockchain.chain.length -1 ].data).toEqual(newData);
+    });
+
+
+});
