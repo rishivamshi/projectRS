eb7c56c86b2b5a50dd1aefd6432cd05e69456b6d
rishivamshi <rishivamshi0079@gmail.com>
2019-02-08 01:42:09 +0530

    all tests successfully passed for nonce and difficulty

--- config.js
+++ config.js
@@ -1,11 +1,18 @@
 // this file contains hardcoded and global values.
 
 // genesis data is the first block of the chain, which has hardcoded values.
+
+// increase the number for more difficulty.
+const INITIAL_DIFFICULTY = 3;
+
 const GENESIS_DATA = {
     timestamp: 1,
     lastHash: '-----',
     hash: 'hash-one',
+    difficulty: INITIAL_DIFFICULTY,
+    nonce: 0,
     data: []
 };
+
 
 module.exports = {GENESIS_DATA};
