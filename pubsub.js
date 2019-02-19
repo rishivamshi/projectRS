d55e10536f08cf730f47645629f5762c9a20f2fa
rishivamshi <rishivamshi0079@gmail.com>
2019-02-19 22:38:26 +0530

    PubNub installed and working

--- pubsub.js
+++ pubsub.js
@@ -1,33 +1,71 @@
-const redis = require('redis');
+// const redis = require('redis');
 
-const CHANNELS = {
+// const CHANNELS = {
+//     TEST: 'TEST'
+// };
+
+// class PubSub {
+//     constructor() {
+//         this.publisher = redis.createClient();
+//         this.subscriber = redis.createClient();
+
+
+//         this.subscriber.subscribe(CHANNELS.TEST);
+
+//         this.subscriber.on(
+//             'message', 
+//             (channel, message) => this.handleMessage(channel,message)
+//         );
+//     }
+//         handleMessage(channel, message) {
+
+//             console.log(`Message received. Channel: ${channel}. Message: ${message}`);
+
+//         }
+    
+// }
+
+// const testPubSub = new PubSub();
+// setTimeout( () => testPubSub.publisher.publish(CHANNELS.TEST, 'foo'), 1000);
+
+const PubNub  = require('pubnub');
+
+const credentials = {
+  publishKey: 'pub-c-9203426b-98c2-4042-8467-5f98e40aea54',
+  subscribeKey: 'sub-c-1e22fa08-3466-11e9-979e-6e9033221872',
+  secretKey: 'sec-c-OTJhNmEwNjItMWNmZS00OTY4LThkMDAtMGM5MTJhMmE2MDM0',
+
+};
+
+const CHANNEL = {
     TEST: 'TEST'
 };
 
 class PubSub {
     constructor() {
-        this.publisher = redis.createClient();
-        this.subscriber = redis.createClient();
-
-
-        this.subscriber.subscribe(CHANNELS.TEST);
-
-        this.subscriber.on(
-            'message', 
-            (channel, message) => this.handleMessage(channel,message)
-        );
+        this.pubnub = new PubNub(credentials);
+        this.pubnub.subscribe({channels : Object.values(CHANNEL) });
+        this.pubnub.addListener( this.listener());
     }
-        handleMessage(channel, message) {
 
-            console.log(`Message received. Channel: ${channel}. Message: ${message}`);
+    listener() {
+        return {
+            message: messageObject => {
+                const {channel, message} = messageObject;
 
-        }
-    
+                console.log(`Message received. Channel : ${channel}. Message: ${message}`);
+            }
+        };
 
+    }
+
+    publish({ channel, message}) {
+        this.pubnub.publish({channel, message});
+    }
 }
 
 
-const testPubSub = new PubSub();
+// const testPubSub = new PubSub();
+// testPubSub.publish({channel: CHANNEL.TEST, message: 'hello pubnub'});
 
-
-setTimeout( () => testPubSub.publisher.publish(CHANNELS.TEST, 'foo'), 6000);
+module.exports = PubSub;
