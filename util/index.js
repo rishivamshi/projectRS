19a8c34d26ab6113b5a8e5143ab740e9bce6e83e
rishivamshi <rishivamshi0079@gmail.com>
2019-02-20 14:36:37 +0530

    Signing data, marking data and verifying signatures to prevent fraud in the system

--- util/index.js
+++ util/index.js
@@ -1,7 +1,14 @@
 const EC = require('elliptic').ec;
+const cryptoHash = require('./crypto-hash');
 
 
 // standards for efficiend cryptography, prime, 256 bits = bitcoin uses this.
 const ec = new EC('secp256k1');
 
-module.exports = {ec};
+const verifySignature = ({publicKey, data, signature}) => {
+    const keyFromPublic = ec.keyFromPublic(publicKey, 'hex');
+
+    return keyFromPublic.verify(cryptoHash(data), signature);
+};
+
+module.exports = {ec, verifySignature};
