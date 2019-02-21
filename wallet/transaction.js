cbd78760ce0ad212e6d5c29807cb576884e3c743
rishivamshi <rishivamshi0079@gmail.com>
2019-02-21 23:33:26 +0530

    Validating Transactions

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,6 +1,6 @@
 // version 1 of uuid is timestamp based.
 const uuid = require('uuid/v1');
-
+const {verifySignature} = require('../util');
 
 class Transaction {
     constructor({ senderWallet, recipient, amount }) {
@@ -25,6 +25,28 @@ class Transaction {
             address: senderWallet.publicKey,
             signature: senderWallet.sign(outputMap)
         };
+    }
+
+    static validTransaction(transaction) {
+
+        const {input, outputMap} = transaction;
+        const {address, amount, signature} = input;
+
+        const outputTotal = Object.values(outputMap)
+            .reduce((total, outputAmount) => total + outputAmount);
+
+        if(amount !== outputTotal) {
+            console.error(`Invalid transaction from ${address}`);
+            return false;
+        }
+
+        if(!verifySignature({ publicKey: address, data: outputMap, signature})) {
+            console.error( `Invalid Signature from ${address}`);
+            return false;
+        }
+        
+
+        return true;
     }
 
 }
