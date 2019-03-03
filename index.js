dcb0149ba508cf000d461a968682d50feffb5755
rishivamshi <rishivamshi0079@gmail.com>
2019-03-03 22:05:16 +0530

    Api endpoint for Mining transactions

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Blockchain = require('./blockchain');
 const PubSub = require('./app/pubsub');
 const TransactionPool = require('./wallet/transaction-pool');
 const Wallet = require('./wallet');
+const TransactionMiner = require('./app/transaction-miner');
 
 
 const app = express();
@@ -13,7 +14,7 @@ const transactionPool = new TransactionPool();
 const wallet = new Wallet();
 
 const pubsub = new PubSub({blockchain, transactionPool, wallet });
-
+const transactionMiner = new TransactionMiner({ blockchain, transactionPool, wallet, pubsub });
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
@@ -67,6 +68,13 @@ app.post('/api/transact', (req, res) => {
 
 app.get('/api/transaction-pool-map', (req, res) => {
     res.json(transactionPool.transactionMap);
+});
+
+app.get('/api/mine-transactions', (req, res) => {
+    transactionMiner.mineTransactions();
+
+    res.redirect('/api/blocks');
+
 });
 
 const syncWithRootState = () => {
