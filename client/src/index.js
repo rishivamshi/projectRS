533f897924f796883479ebc1f3dc58781d8bbf8b
rishivamshi <rishivamshi0079@gmail.com>
2019-03-27 23:37:07 +0530

    frontend development workflow

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,6 @@ import { render } from 'react-dom';
 
 
 render(
-    <div>Social network in React!</div>,
+    <div>Social network in React!: Live updatesssss</div>,
     document.getElementById('root')
 );
