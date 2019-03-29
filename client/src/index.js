3ed8949fefb4b18f5fbb5a6024779715ef5da133
rishivamshi <rishivamshi0079@gmail.com>
2019-03-30 00:39:58 +0530

    App component created

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './components/App';
 
 
 render(
-    <div>Social network in React!: Live updatesssss</div>,
+    <App />,
     document.getElementById('root')
 );
